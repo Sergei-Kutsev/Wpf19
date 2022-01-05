dca9971e684862cfd28f7b4c802c44de0af77c23
Сергей Куцев <sergeikutsev@yandex.ru>
2022-01-05 22:54:29 +0300

    Добавьте файлы проекта.

---
+++ Wpf19/Model/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf19.Model
+{
+    static class Ariph
+    {
+        public static double Add(double a) => Math.Round(a* 2 * Math.PI, 2);
+    }
+}
